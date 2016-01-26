1da2bd024b27ad25396339dc331b1229bfbe9c27
alancerio18 <alancerio18@alex>
2016-01-26 14:53:06 -0600

    all done

---
+++ src/device/EletronicDevice.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package device;
+
+/**
+ *
+ * @author alancerio18
+ */
+public abstract class EletronicDevice extends Device {
+    
+    public void charger(){
+        System.out.println("Charge device");
+    }
+}
---
+++ src/device/Start.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package device;
+
+/**
+ *
+ * @author alancerio18
+ */
+public class Start {
+    public static void main(String[] args) {
+        Samsung h = new Samsung();
+        
+        System.out.println(h.name());
+    }
+}
